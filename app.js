99f02027be5642d9215c2ceed90f0400840bf405
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-20 12:06:04 +0300

    fix: add app PORT

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
+const PORT = 4000
 
 db.sync();
-app.use(require('body-parser'));
+app.use(bodyParser.json());
+
+app.use('/', (req, res, next) => {
+    if (req.originalUrl === '/') {
+        res.send('Service is running!');
+        return;
+    }
+    next();
+});
+
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
+app.listen(PORT, function() {
+    console.log(`App is listening on ${PORT}`);
 })
