4528ef5e32ab7f7ae5bff90c67a19b3626dd7ef8
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-22 00:17:02 +0300

    fix: game controller

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var Game = require('../db').import('../models/game');
 
 router.get('/all', (req, res) => {
-    Game.findAll()
+    Game.findAll({ where: { owner_id: req.user.id } })
         .then(
             function findSuccess(games) {
                 res.status(200).json({
@@ -20,7 +20,7 @@ router.get('/all', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    Game.findOne({ where: { id: req.params.id } })
+    Game.findOne({ where: { id: req.params.id, owner_id: req.user.id } })
         .then(
             function findSuccess(game) {
                 res.status(200).json({
@@ -39,7 +39,7 @@ router.get('/:id', (req, res) => {
 router.post('/create', (req, res) => {
     Game.create({
         title: req.body.game.title,
-        owner_id: req.body.user.id,
+        owner_id: req.user.id,
         studio: req.body.game.studio,
         esrb_rating: req.body.game.esrb_rating,
         user_rating: req.body.game.user_rating,
@@ -70,6 +70,7 @@ router.put('/update/:id', (req, res) => {
         {
             where: {
                 id: req.params.id,
+                owner_id: req.user.id
             }
         })
         .then(
@@ -93,6 +94,7 @@ router.delete('/remove/:id', (req, res) => {
     Game.destroy({
         where: {
             id: req.params.id,
+            owner_id: req.user.id
         }
     })
     .then(
