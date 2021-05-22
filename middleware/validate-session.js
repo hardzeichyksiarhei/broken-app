b01aa91a12683c1c66fdad2d6214b99691eeb0bd
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-22 19:23:59 +0300

    ref: validation session middleware

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,32 +1,38 @@
+const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
-var User = require('../db').import('../models/user');
 
-module.exports = function (req, res, next) {
-    if (req.method == 'OPTIONS') {
-        next();   // allowing options as a method for request
-    } else {
-        var sessionToken = req.headers.authorization;
-        if (sessionToken && sessionToken.indexOf('Bearer ') === 0) {
-            sessionToken = sessionToken.slice(7, sessionToken.length)
-        }
-        console.log(sessionToken);
-        if (!sessionToken) return res.status(403).send({ auth: false, message: "No token provided." });
-        else {
-            jwt.verify(sessionToken, 'lets_play_sum_games_man', (err, decoded) => {
-                if (decoded) {
-                    User.findOne({ where: { id: decoded.id } }).then(user => {
-                        req.user = user;
-                        console.log(`user: ${user}`)
-                        next()
-                    },
-                        function () {
-                            res.status(401).send({ error: "not authorized" });
-                        })
+const User = require('../db').import('../models/user');
 
-                } else {
-                    res.status(400).send({ error: "not authorized" })
-                }
-            });
-        }
+module.exports = async (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        return next(); // allowing options as a method for request
+    } 
+    
+    let sessionToken = req.headers.authorization;
+    if (sessionToken && sessionToken.indexOf('Bearer ') === 0) {
+        sessionToken = sessionToken.slice(7, sessionToken.length)
     }
+
+    if (!sessionToken) {
+        return res.status(StatusCodes.UNAUTHORIZED).send({
+            auth: false,
+            message: "Not authorized to access this resource. Auth token is not supplied"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(sessionToken, 'lets_play_sum_games_man')
+
+        if (!decoded || !decoded.id) throw new Error('Not authorized to access this resource. Token is not valid');
+        
+        const user = await User.findOne({ where: { id: decoded.id } });
+        if (!user) throw new Error('Not authorized to access this resource. Token is not valid');
+        
+        req.user = user;
+        console.log(`User: ${user}`)
+    } catch (error) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ auth: false, error: error.message })
+    } 
+
+    return next()
 }
